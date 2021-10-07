1be3681ccc9d9ec8d204613ea1fae96bb56c1f75
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2021-10-07 18:31:28 +0300

    merge App launch test branch
    * change import diskcache queue
    
    * add diskcache to repo root requirements.txt
    
    * add sys paths
    
    * correct log for paths
    
    * correct log for paths
    
    * modal window
    
    * better descriptio for modal window selectors
    
    * correct default parameters in config.json
    
    * correct item caching
    
    * probabilities threshold slider
    
    * probabilities threshold slider
    
    * new model links
    
    * try cuda device
    
    * try cuda device

--- supervisely/src/sly_globals.py
+++ supervisely/src/sly_globals.py
@@ -13,6 +13,14 @@ WORKSPACE_ID = int(os.environ['context.workspaceId'])
 PROJECT_ID = int(os.environ["modal.state.slyProjectId"])
 
 
+# DEVICE
+# devices: cpu, cuda, xpu, mkldnn, opengl, opencl,
+# ideep, hip, msnpu, mlc, xla, vulkan, meta, hpu
+DEVICE = os.environ["modal.state.device"]
+MODEL = int(os.environ["modal.state.model"])
+BRS_MODE = int(os.environ["modal.state.brs_mode"])
+PREDICTIONS_THRESHOLD = float(os.environ["modal.state.prob_thresh"])
+
 work_dir = os.path.join(my_app.data_dir, "work_dir")
 mkdir(work_dir, True)
 
@@ -20,22 +28,17 @@ img_dir = os.path.join(work_dir, "img")
 
 
 # Create Cache
-cache_dir = my_app.cache_dir
+cache_dir = os.path.join(work_dir, "diskcache")
 cache = Cache(directory=cache_dir)
-cache_item_limit = 30
+cache_item_expire_time = 600  # seconds
 mkdir(cache_dir)
 mkdir(img_dir)
-
-# DEVICE
-# devices: cpu, cuda, xpu, mkldnn, opengl, opencl,
-# ideep, hip, msnpu, mlc, xla, vulkan, meta, hpu
-DEVICE = "cpu"
 
 
 # LOAD MODEL
 import torch
 import requests
-import dl_progress
+import download_progress
 from supervisely_lib.io.fs import download
 from isegm.inference.utils import load_is_model
 
@@ -43,39 +46,39 @@ from isegm.inference.utils import load_is_model
 def download_file_from_link(api, link, model_path, file_name, progress_message, app_logger):
     response = requests.head(link, allow_redirects=True)
     sizeb = int(response.headers.get('content-length', 0))
-    progress_cb = dl_progress.get_progress_cb(api, TASK_ID, progress_message, sizeb, is_size=True)
+    progress_cb = download_progress.get_progress_cb(api, TASK_ID, progress_message, sizeb, is_size=True)
     download(link, model_path, cache=my_app.cache, progress=progress_cb)
-    dl_progress.reset_progress(api, TASK_ID)
+    download_progress.reset_progress(api, TASK_ID)
     app_logger.info(f'{file_name} has been successfully downloaded')
 
 
-model_link = "https://github.com/saic-vul/ritm_interactive_segmentation/releases/download/v1.0/coco_lvis_h32_itermask.pth"
-model_name = "coco_lvis_h32_itermask.pth"
+# MODEL SELECTOR
+available_models = [
+    "https://github.com/supervisely-ecosystem/ritm-interactive-segmentation/releases/download/v0.1/sbd_h18_itermask.pth",
+    "https://github.com/supervisely-ecosystem/ritm-interactive-segmentation/releases/download/v0.1/coco_lvis_h18_baseline.pth",
+    "https://github.com/supervisely-ecosystem/ritm-interactive-segmentation/releases/download/v0.1/coco_lvis_h18s_itermask.pth",
+    "https://github.com/supervisely-ecosystem/ritm-interactive-segmentation/releases/download/v0.1/coco_lvis_h18_itermask.pth",
+    "https://github.com/supervisely-ecosystem/ritm-interactive-segmentation/releases/download/v0.1/coco_lvis_h32_itermask.pth"
+]
+
+
+model_link = available_models[MODEL]
+model_name = os.path.basename(os.path.normpath(model_link))
 model_dir = os.path.join(work_dir, "model")
 mkdir(model_dir)
 model_path = os.path.join(model_dir, model_name)
 
 download_file_from_link(api, model_link, model_path, model_name, f"Download {model_name}", my_app.logger)
 
-model = torch.load(model_path, map_location=torch.device(DEVICE))
-model = load_is_model(model, DEVICE)
+
+MODEL = torch.load(model_path, map_location=torch.device(DEVICE))
+MODEL = load_is_model(MODEL, DEVICE)
 
 
 # RITM CONTROLLER
 from interactive_demo.controller import InteractiveController
-# brs_modes = ['NoBRS', 'RGB-BRS', 'DistMap-BRS', 'f-BRS-A', 'f-BRS-B', 'f-BRS-C']
+available_brs_modes = ['NoBRS', 'RGB-BRS', 'DistMap-BRS', 'f-BRS-A', 'f-BRS-B', 'f-BRS-C']
 
-brs_mode = "NoBRS"
+brs_mode = available_brs_modes[BRS_MODE]
 predictor_params = {'brs_mode': brs_mode}
-controller = InteractiveController(model, DEVICE, predictor_params)
-
-
-
-# MODEL SELECTOR
-# models = {
-#             "HRNet18 IT-M (SDB)": "https://github.com/saic-vul/ritm_interactive_segmentation/releases/download/v1.0/sbd_h18_itermask.pth",
-#             "HRNet18 (COCO+LVIS)": "https://github.com/saic-vul/ritm_interactive_segmentation/releases/download/v1.0/coco_lvis_h18_baseline.pth",
-#             "HRNet18s IT-M (COCO+LVIS)": "https://github.com/saic-vul/ritm_interactive_segmentation/releases/download/v1.0/coco_lvis_h18s_itermask.pth",
-#             "HRNet18 IT-M (COCO+LVIS)": "https://github.com/saic-vul/ritm_interactive_segmentation/releases/download/v1.0/coco_lvis_h18_itermask.pth",
-#             "HRNet32 IT-M (COCO+LVIS)": "https://github.com/saic-vul/ritm_interactive_segmentation/releases/download/v1.0/coco_lvis_h32_itermask.pth"
-# }
+controller = InteractiveController(MODEL, DEVICE, predictor_params, prob_thresh=PREDICTIONS_THRESHOLD)
--- supervisely/src/sly_main.py
+++ supervisely/src/sly_main.py
@@ -1,21 +1,27 @@
 import os
 import functools
 import supervisely_lib as sly
+
+# ROOTS
+import sys
+from pathlib import Path
+
+repo_root_source_dir = str(Path(sys.argv[0]).parents[2])
+sly.logger.info(f"Repo root source directory: {repo_root_source_dir}")
+sys.path.append(repo_root_source_dir)
+
+app_root_source_dir = str(Path(sys.argv[0]).parents[1])
+sly.logger.info(f"App root source directory: {app_root_source_dir}")
+sys.path.append(app_root_source_dir)
+
+sources_dir = str(Path(sys.argv[0]).parents[0])
+sly.logger.info(f"Source directory: {sources_dir}")
+sys.path.append(sources_dir)
+
+
 import sly_globals as g
 import mask_image
 import sly_functions as f
-
-# ROOTS
-# import sys
-# from pathlib import Path
-#
-# root_source_dir = str(Path(sys.argv[0]).parents[1])
-# sly.logger.info(f"Root source directory: {root_source_dir}")
-# sys.path.append(root_source_dir)
-#
-# sly_sources_dir = str(Path(sys.argv[0]).parents[0])
-# sly.logger.info(f"Source directory: {sly_sources_dir}")
-# sys.path.append(sly_sources_dir)
 
 
 def send_error_data(func):
