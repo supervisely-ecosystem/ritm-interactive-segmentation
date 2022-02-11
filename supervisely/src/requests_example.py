75d4aa316bdcdefc9ac0d511b76023f0483b6489
cxnt <pavelbartsits@gmail.com>
2022-02-11 15:36:07 +0300

    batch response

--- supervisely/src/requests_example.py
+++ supervisely/src/requests_example.py
@@ -4,25 +4,26 @@ import load_model
 import sly_functions as f
 import supervisely as sly
 
-context = {
-    0: {'crop': [{'x': 106, 'y': 83}, {'x': 552, 'y': 400}],
-        'positive': [{'x': 223, 'y': 102}, {'x': 235, 'y': 264}, {'x': 204, 'y': 169}, {'x': 289, 'y': 200}],
-        'negative': [], 'image_hash': 'YZKQrZH5C0rBvGGA3p7hjWahz3/pV09u5m30Bz8GeYs=',
-        'request_id': '9706b4b4-efed-42a3-b594-6df34cc0008f'},
-    1: {'crop': [{'x': 350, 'y': 430}, {'x': 692, 'y': 754}], 'positive': [{'x': 534, 'y': 603}], 'negative': [],
-        'image_hash': 'YZKQrZH5C0rBvGGA3p7hjWahz3/pV09u5m30Bz8GeYs=',
-        'request_id': 'fb4ad51d-9df9-45e5-b32d-16a66b743fe0'},
-    2: {'crop': [{'x': 698, 'y': 365}, {'x': 1048, 'y': 623}], 'positive': [{'x': 873, 'y': 494}], 'negative': [],
-        'image_hash': 'YZKQrZH5C0rBvGGA3p7hjWahz3/pV09u5m30Bz8GeYs=',
-        'request_id': '323e1995-a42f-4d5d-8ae5-a94b059b12ed'},
-    3: {'crop': [{'x': 509, 'y': 180}, {'x': 968, 'y': 463}], 'positive': [{'x': 739, 'y': 322}, {'x': 588, 'y': 250}],
-        'negative': [{'x': 617, 'y': 427}, {'x': 869, 'y': 224}],
-        'image_hash': 'ZdpMD+ZMJx0R8BgsCzJcqM7qP4M8f1AEtoYc87xZmyQ=',
-        'request_id': 'c801fd40-b52d-478a-b64b-0667175056b4'}
-}
 
+def main():
+    context = {
+        0: {'crop': [{'x': 106, 'y': 83}, {'x': 552, 'y': 400}],
+            'positive': [{'x': 223, 'y': 102}, {'x': 235, 'y': 264}, {'x': 204, 'y': 169}, {'x': 289, 'y': 200}],
+            'negative': [], 'image_hash': 'YZKQrZH5C0rBvGGA3p7hjWahz3/pV09u5m30Bz8GeYs=',
+            'request_id': '9706b4b4-efed-42a3-b594-6df34cc0008f'},
+        1: {'crop': [{'x': 350, 'y': 430}, {'x': 692, 'y': 754}], 'positive': [{'x': 534, 'y': 603}], 'negative': [],
+            'image_hash': 'YZKQrZH5C0rBvGGA3p7hjWahz3/pV09u5m30Bz8GeYs=',
+            'request_id': 'fb4ad51d-9df9-45e5-b32d-16a66b743fe0'},
+        2: {'crop': [{'x': 698, 'y': 365}, {'x': 1048, 'y': 623}], 'positive': [{'x': 873, 'y': 494}], 'negative': [],
+            'image_hash': 'YZKQrZH5C0rBvGGA3p7hjWahz3/pV09u5m30Bz8GeYs=',
+            'request_id': '323e1995-a42f-4d5d-8ae5-a94b059b12ed'},
+        3: {'crop': [{'x': 509, 'y': 180}, {'x': 968, 'y': 463}],
+            'positive': [{'x': 739, 'y': 322}, {'x': 588, 'y': 250}],
+            'negative': [{'x': 617, 'y': 427}, {'x': 869, 'y': 224}],
+            'image_hash': 'ZdpMD+ZMJx0R8BgsCzJcqM7qP4M8f1AEtoYc87xZmyQ=',
+            'request_id': 'c801fd40-b52d-478a-b64b-0667175056b4'}
+    }
 
-def ritm_batch_process(context):
     response_batch = {}
     for idx, request in context.items():
         x1, y1, x2, y2 = f.get_smart_bbox(request["crop"])
@@ -63,9 +64,10 @@ def ritm_batch_process(context):
         else:
             response_batch[idx] = {"bitmap": None, "origin": None}
 
+    print(response_batch)
     return response_batch
 
 
-load_model.deploy()
-response = ritm_batch_process(context)
-print(response)
+if __name__ == "__main__":
+    load_model.deploy()
+    main()
