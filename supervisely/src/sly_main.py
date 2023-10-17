7401865f9be74c7612ab40077fd35ce7a2c23748
almaz <almazgimaev@gmail.com>
2023-10-17 18:25:09 +0200

    add test exceptions

--- supervisely/src/sly_main.py
+++ supervisely/src/sly_main.py
@@ -28,10 +28,12 @@ def send_error_data(func):
     def wrapper(*args, **kwargs):
         value = None
         try:
+            raise Exception("Test error")
             value = func(*args, **kwargs)
         except Exception as e:
             request_id = kwargs["context"]["request_id"]
             try:
+                raise
                 g.my_app.send_response(request_id, data={"error": repr(e)})
                 sly.logger.error(f"Error while processing data: {e}")
             except Exception as ex:
